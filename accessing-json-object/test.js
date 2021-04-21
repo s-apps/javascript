707bf938497ebabdda73a68083a59a7b50910726
Silverio O Paula <contato@silverio.eti.br>
2021-04-21 12:52:56 -0300

    first commit

---
+++ accessing-json-object/test.js
@@ -0,0 +1,25 @@
+var collection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  function updateRecords(object, id, prop, value) {
+    return object;
+  }
+  
+  console.log(updateRecords(collection, 5439, 'artist', 'ABBA'));
