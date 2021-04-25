825580db5ea0b2f0fc5ac67c7689ec3cc068c733
Silverio O Paula <contato@silverio.eti.br>
2021-04-25 19:12:32 -0300

    es6 declare a read-only variable with the const keyword

---
+++ ES6/declare_read-only_variable_with_const_keyword/test.js
@@ -0,0 +1,17 @@
+//const FAV_PET = 'Cats';
+//FAV_PET = 'Dogs';
+
+/*
+Change the code so that all variables are declared using let or const. 
+Use let when you want the variable to change, and const when you want the variable to remain constant. 
+Also, rename variables declared with const to conform to common practices, meaning constants should be in 
+all caps.
+*/
+
+function printManyTimes(str) {
+    const SENTENCE = str + ' is cool!';
+    for (let i = 0; i < str.length; i+=2) {
+      console.log(SENTENCE);
+    }
+}
+printManyTimes('freeCodeCamp');
