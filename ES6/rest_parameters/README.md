77f547eb722a8fb148d6e9add598b9223c47ec36
Silverio O Paula <contato@silverio.eti.br>
2021-04-25 21:27:43 -0300

    es6 rest parameter

---
+++ ES6/rest_parameters/README.md
@@ -0,0 +1,2 @@
+#  Rest parameters
+The rest parameter syntax allows a function to accept an indefinite number of arguments as an array.
---
+++ ES6/rest_parameters/use_rest_parameter_with_function_parameters/test.js
@@ -0,0 +1,30 @@
+function howMany(...args) {
+    return 'You have passed ' + args.length + ' arguments.';
+}
+console.log(howMany(0, 1, 2));
+console.log(howMany('string', null, [1, 2, 3], {}));
+
+/*-----------------------------------------------------------------*/
+
+const sum = (x, y, z) => {
+    const args = [x, y, z];
+    return args.reduce((a, b) => a + b, 0);
+}
+
+console.log(sum(0, 1, 2));
+console.log(sum(1, 2, 3, 4));
+console.log(sum(5));
+console.log(sum());
+
+/*
+Modify the function sum using the rest parameter in such a way that the function sum is able to take
+any number of arguments and return their sum.
+*/
+
+const sumRestParameter = (...args) => {
+    return args.reduce((a, b) => a + b, 0);        
+}
+console.log('rest parameter', sumRestParameter(0, 1, 2));
+console.log('rest parameter', sumRestParameter(1, 2, 3, 4));
+console.log('rest parameter', sumRestParameter(5));
+console.log('rest parameter', sumRestParameter());
