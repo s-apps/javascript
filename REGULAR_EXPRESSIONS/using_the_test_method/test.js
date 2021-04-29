3c1a693c8f8db3b1aaaee62475e39470d057a6d3
Silverio O Paula <contato@silverio.eti.br>
2021-04-28 21:31:26 -0300

    regex using the test method

--- REGULAR_EXPRESSIONS/using_the_test_method/test.js
+++ REGULAR_EXPRESSIONS/using_the_test_method/test.js
@@ -4,5 +4,5 @@ Apply the regex myRegex on the string myString using the .test() method.
 
 let myString = "Hello, World!";
 let myRegex = /Hello/;
-let result = myRegex.test(myRegex); // Change this line
+let result = myRegex.test(myString); // Change this line
 console.log(result);
